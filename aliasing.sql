7d921ce010d1e76005bff3bdbb23ec5d879c0f94
foziaShareen <foziashareen1@gmail.com>
2023-12-14 10:29:37 +0500

    Update aliasing.sql
    dont use alias for column name in where clause

--- aliasing.sql
+++ aliasing.sql
@@ -3,3 +3,7 @@ from `movies` as m
 -- we may name alias for columns 
 SELECT  m.movie_title  as t,m.year as y
 from `movies` as m
+-- note when we have to use where clause we have to put column full name not aliase but can put alias of table for example
+SELECT  m.movie_title  as t,m.year as y
+from `movies` as m 
+where m.movie_title like '%e%';
