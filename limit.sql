02497b8ae533420a0d16bcd5b17eb902196a0f7f
foziaShareen <foziashareen1@gmail.com>
2023-12-14 11:24:58 +0500

    Update limit.sql
    how many and from where to start

--- limit.sql
+++ limit.sql
@@ -3,3 +3,9 @@ from `movies`
 where year>=2007
 order by year DESC
 limit 5
+-- we can specify from where to start and upto how many of record we want to display
+select movie_title, year
+from `movies`
+order by movie_id
+limit 6,5
+-- starting after 6 means starting from number 7 and the total record 5
