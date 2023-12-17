52531c5beff3215a79d36b1b482f9fa416af54ae
foziaShareen <foziashareen1@gmail.com>
2023-12-17 19:11:57 +0500

    Create replace.sql
    replace in sql

---
+++ replace.sql
@@ -0,0 +1,4 @@
+-- replace will udate if values alredy exists if does not exist it will create it 
+--combo of insert and update command
+REPLACE INTO `people`(`email`, `first_name`, `last_name`, `common_name`)
+VALUES ('brandon@stark.org', 'Brandon', 'Stark', 'fozia');
